1d44b13b0f71c21b0a825d4132ecae058a1bab10
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-04-20 18:53:27 +0100

    Animations added

--- src/app/components/SpeechBubble/SpeechBubble.tsx
+++ src/app/components/SpeechBubble/SpeechBubble.tsx
@@ -1,7 +1,9 @@
+'use client'
 import styles from './SpeechBubble.module.scss'
 import CodeSnippet from "../CodeSnippet/CodeSnippet"
 import Link from "next/link";
 import TimeStamp from "../TimeStamp/TimeStamp";
+import { motion } from "framer-motion";
 
 function SpeechBubble(props :
                           {   message : string,
@@ -15,20 +17,27 @@ function SpeechBubble(props :
                               codeStyle ? : string
                           }) {
     return(
-        <main
+        <motion.div
             className={styles.speechBubble}
             style={{backgroundColor:props.bgColor
                     || "#F7941D",
                     alignSelf:props.align
                     || "center"
-            }}>
+            }}
+            drag
+            dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
+            dragElastic={0.5}
+            whileHover={{ cursor : "grab", zIndex: 1 }}
+            whileTap={{ cursor: "grabbing", scale: 1.2 }}
+        >
             {props.message}
             <Link style={{color : props.linkColor || "#fff"}} className={styles.link} href="/">{props.home}</Link>
             <Link style={{color : props.linkColor || "#fff"}} className={styles.link} href="/typescript/">{props.typescript}</Link>
             <br/>
             {props.code && <CodeSnippet code={props.code} codeStyle={props.codeStyle} />}
             <TimeStamp user={props.user} />
-        </main>
+        </motion.div>
     )
 }
 
--- src/app/components/TimeStamp/TimeStamp.tsx
+++ src/app/components/TimeStamp/TimeStamp.tsx
@@ -1,9 +1,18 @@
+'use client'
 import styles from './TimeStamp.module.scss'
+import { motion } from 'framer-motion'
 
 function TimeStamp(props : { user : string }) {
     return(
         <main className={styles.timeStamp}>
-            <span className={styles.user}><span className={styles.indicator}/>{props.user}</span>
+            <span className={styles.user}>
+                <motion.span
+                    animate={{scale:1.2}}
+                    initial={{scale:1}}
+                    transition={{ease: "linear", duration: 2, repeat: Infinity, repeatType: "reverse" }}
+                    className={styles.indicator}
+                />{props.user}
+            </span>
         </main>
     )
 }
