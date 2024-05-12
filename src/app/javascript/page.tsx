2f72afbb88d7049edb3c637c7056a025bc35916e
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-05-12 15:11:59 +0100

    Added Buttons Documentation Up to date

--- src/app/javascript/page.tsx
+++
@@ -1,6 +0,0 @@
-import SpeechBubble from "@/app/components/SpeechBubble/SpeechBubble";
-import styles from "./javascript.module.scss";
-
-export default function Typescript() {
-	return <main className={styles.main} />;
-}
---
+++ src/app/pandared/page.tsx
@@ -0,0 +1,15 @@
+import styles from "./pandaRedUi.module.scss";
+
+export default function Typescript() {
+	return (
+		<main className={styles.main}>
+			<iframe
+				title="Red Panda UI"
+				src="https://664099e473cee1d4b7313e8f-vrzsueqaho.chromatic.com/iframe.html?path=/docs/pandaui-button--docs"
+				width="100%"
+				height="100%"
+				style={{ border: "none" }}
+			/>
+		</main>
+	);
+}
