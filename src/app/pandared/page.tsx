6b3792163a0e60e62eaa6b492b07d426d9c9d632
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-05-16 20:47:27 +0100

    Vulnerability in SASS checked

--- src/app/pandared/page.tsx
+++ src/app/pandared/page.tsx
@@ -3,7 +3,7 @@ export default function PandaUI() {
 		<main className="uiWrapper">
 			<iframe
 				title="Red Panda UI"
-				src="https://664099e473cee1d4b7313e8f-apxgtgpjug.chromatic.com/?path=story/docs/pandaui-button--docs"
+				src="https://664099e473cee1d4b7313e8f-ntyiuyjcbc.chromatic.com/"
 				width="100%"
 				height="100%"
 				style={{ border: "none", overflow: "hidden" }}
