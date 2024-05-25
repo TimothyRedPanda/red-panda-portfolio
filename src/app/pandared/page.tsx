84cd36a6ec258120ef53465f18ef0e6cabf5d9f4
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-05-25 21:31:02 +0100

    Fixed UI display

--- src/app/pandared/page.tsx
+++ src/app/pandared/page.tsx
@@ -1,12 +1,13 @@
+import styles from "./index.module.scss";
 export default function PandaUI() {
 	return (
-		<main className="uiWrapper">
+		<main className={styles.uiWrapper}>
 			<iframe
 				title="Red Panda UI"
 				src="https://664099e473cee1d4b7313e8f-ntyiuyjcbc.chromatic.com/"
 				width="100%"
 				height="100%"
-				style={{ border: "none", overflow: "hidden" }}
+				style={{ border: "none" }}
 			/>
 		</main>
 	);
