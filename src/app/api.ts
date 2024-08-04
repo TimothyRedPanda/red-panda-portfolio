bea93c3fa8f92bec37a85d0166be763fd5ca2d5d
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-08-04 16:31:23 +0100

    Had to fix any type error in api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -4,7 +4,7 @@ const API = axios.create({
 	baseURL: "https://emkc.org/api/v2/piston",
 });
 
-async function executeCode(sourceCode) {
+async function executeCode(sourceCode: string) {
 	const response = await API.post("/execute", {
 		language: "js",
 		version: "18.15.0",
