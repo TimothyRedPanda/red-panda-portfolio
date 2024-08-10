c94d966645f6876a7d5d71ff68254c5cecab0704
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-08-10 19:19:06 +0100

    Fixed code display on Red Panda UI page

--- src/app/redpandaui/page.tsx
+++ src/app/redpandaui/page.tsx
@@ -11,21 +11,22 @@ const buttonCode = `// Examples of how these Buttons are being used on this very
 import { Button } from "ui-redpanda";
 import "ui-redpanda/dist/style.css";
 
-const ExampleUI = () => {
-	return (
-		<main className="w-dvw h-dvh flex flex-col items-center mt-10">
-			<section className="flex items-center flex-col md:flex-row gap-1 border-4 p-5 shadow-questionShadow">
-				<h1>Example Buttons:</h1>
-				<Button>Default</Button>
-				<Button className="outlined">Outlined</Button>
-				<Button className="contained">Contained</Button>
-				<Button className="disabled">Disabled</Button>
-				<Button onClick={() => setClicked(clicked + 1)}>
-					Clicked: {clicked}
-				</Button>
-			</section>
-		</main>
-	);
+const ExampleUI = () => {	
+
+return (
+<main className="w-dvw h-dvh flex flex-col items-center mt-10">
+	<section className="flex items-center flex-col md:flex-row gap-1 border-4 p-5 shadow-questionShadow">
+		<h1>Example Buttons:</h1>
+		<Button>Default</Button>
+		<Button className="outlined">Outlined</Button>
+		<Button className="contained">Contained</Button>
+		<Button className="disabled">Disabled</Button>
+		<Button onClick={() => setClicked(clicked + 1)}>
+			Clicked: {clicked}
+		</Button>
+	</section>
+</main>
+);		
 };`;
 
 const ExampleUI = () => {
@@ -40,7 +41,7 @@ const ExampleUI = () => {
 					Red Panda UI
 				</h1>
 			</Link>
-			<section className="flex items-center flex-col md:flex-row gap-1 border-4 p-5 shadow-questionShadow w-3/4">
+			<section className="flex items-center flex-col md:flex-row gap-4 border-4 p-5 shadow-questionShadow w-3/4">
 				<h1>Example Buttons:</h1>
 				<Button>Default</Button>
 				<Button className="outlined">Outlined</Button>
