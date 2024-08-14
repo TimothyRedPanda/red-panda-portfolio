181a4a149eedc8457918c8cf91d8b33bde0148dc
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-08-14 16:40:58 +0100

    Added Switch and Label to the Red Panda UI docs page

--- src/app/components/ExampleUI/ExampleUI.tsx
+++ src/app/components/ExampleUI/ExampleUI.tsx
@@ -1,13 +1,19 @@
 "use client";
-import { Button } from "ui-redpanda";
+import { Button, Label, Switch } from "ui-redpanda";
 import "ui-redpanda/dist/style.css";
 import PrismLoader from "../PrismLoader/PrismLoader";
 import Link from "next/link";
 
-const buttonCode = `<Button>Default</Button>
+const code = `// Examples of button variants:
+<Button>Default</Button>
 <Button variant="outlined">Outlined</Button>
 <Button variant="contained">Contained</Button>
-<Button variant="disabled">Disabled</Button>`;
+<Button variant="disabled">Disabled</Button>
+// Examples of label and switch variants as well as use of the optional className prop:
+<Label className="justify-self-end" size="large" label="Live switch:"/> 
+<Switch className="justify-self-start" variant="live"/>
+<Label className="justify-self-end" size="large" label="Disabled switch:"/>
+<Switch className="justify-self-start" variant="disabled"/>`;
 
 const importCode = `npm i ui-redpanda 
 import { /* UI component name here */ } from "ui-redpanda"
@@ -21,21 +27,33 @@ const ExampleUI = () => {
 					Red Panda UI
 				</h1>
 			</Link>
-			<pre>
+			<pre className="w-3/4">
 				<code className="language-bash">{importCode}</code>
 			</pre>
-			<section className="flex items-center gap-1 flex-col border-4 border-slate-300 p-4 shadow-questionShadow w-3/4 rounded-md wrapper">
-				<section className="flex flex-col md:justify-around md:w-full md:flex-row gap-3">
+			<section className="flex flex-col border-slate-300 p-4 shadow-questionShadow w-3/4 rounded-md wrapper">
+				<section className="grid grid-cols-1 md:grid-cols-4 gap-5 items-center justify-items-center w-full h-fit p-4">
 					<Button>Default</Button>
 					<Button variant="outlined">Outlined</Button>
 					<Button variant="contained">Contained</Button>
 					<Button variant="disabled">Disabled</Button>
+					<Label
+						className="sm:justify-self-end"
+						size="large"
+						label="Live switch:"
+					/>
+					<Switch className="sm:justify-self-start " variant="live" />
+					<Label
+						className="sm:justify-self-end"
+						size="large"
+						label="Disabled switch:"
+					/>
+					<Switch className="sm:justify-self-start" variant="disabled" />
 				</section>
 				<span className="divide-wrap self-center w-full m-8 relative h-[1.5px]">
 					<span className="divider opacity-0 absolute w-px h-full" />
 				</span>
 				<pre className="w-full">
-					<code className="language-jsx">{buttonCode}</code>
+					<code className="language-jsx">{code}</code>
 				</pre>
 			</section>
 			<PrismLoader />
