290c88b1b9d13147d8e184c54c0b8b8fc7fb8521
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-08-29 00:38:21 +0100

    noStore Added

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,6 @@
+import { unstable_noStore as noStore } from "next/cache";
 import { openai } from "@ai-sdk/openai";
+
 import { streamText, convertToCoreMessages } from "ai";
 import { content } from "./content";
 
@@ -6,6 +8,7 @@ export const dynamic = "force-dynamic";
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
+	noStore();
 	const { messages } = await req.json();
 
 	const result = await streamText({
